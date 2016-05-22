7f5ca57855519d74654eff19886d979ae31daf89
vkmp3mod <krosovscky.maxim@yandex.ru>
2016-05-22 03:22:40 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// Optional jQuery class toggle for added robustness. 
+
+/* 
+var labelID;
+
+$('label').click(function() {
+  labelID = $(this).attr('for');
+  $('#' + labelID).toggleClass('active');
+}); 
+*/
